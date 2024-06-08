0b87533eaa6e7bb51e1f37125f111176ac4bb589
Surprised Pika <surprisedpikaaaaaaa@gmail.com>
2024-06-08 20:36:03 +0100

    a

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    let mut memory: [u8; 0x298 * 420 * 2] = [0; 0x298 * 420 * 2];
+    //Todo: kid named memory
+}
+
+enum PouchItemType {
+    Sword = 0,
+    Bow = 1,
+    Arrow = 2,
+    Shield = 3,
+    ArmorHead = 4,
+    ArmorUpper = 5,
+    ArmorLower = 6,
+    Material = 7,
+    Food = 8,
+    KeyItem = 9,
+    Invalid = -1
+}
+
+struct PouchItem {
+    address: i8
 }
