8e440fef713a10a3aeaeb835b9388fc70d1e6dc4
Surprised Pika <surprisedpikaaaaaaa@gmail.com>
2024-06-10 15:28:38 +0100

    Check size is consistent

--- src/fs.rs
+++ src/fs.rs
@@ -1,11 +1,17 @@
 use std::fs::File;
 use std::io::Read;
 
+use crate::PauseMenuDataMgr;
+
 pub fn get_pmdm(path: &str) -> Option<Vec<u8>> {
     // read pmdm file
     let mut file = File::open(path).ok()?;
     let mut buffer: Vec<u8> = Vec::new();
     file.read_to_end(&mut buffer).ok()?;
+
+    if buffer.len() != std::mem::size_of::<PauseMenuDataMgr>() {
+        panic!("File size not the same as PMDM size");
+    }
 
     Some(buffer)
 }
